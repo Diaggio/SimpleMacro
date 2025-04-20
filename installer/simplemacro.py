c003242d37ce90fc8f9bbb09ed114154ff0277d5
Diaggio <123846531+Diaggio@users.noreply.github.com>
2025-04-20 16:48:00 -0700

    changed to ttk buttons / finishing keyboard handling

--- installer/simplemacro.py
+++ installer/simplemacro.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from pynput import mouse,keyboard
 import queue
 import time
+import threading
 
 
 
@@ -9,6 +11,11 @@ class MouseMacro:
     def __init__(self,root):
         self.root = root
         self.root.title("Simple Macro")
+        self.root.configure(bg="#23272e")
+
+        self.style = ttk.Style()
+        self.style.configure("RedText.TButton",foreground="red")
+        self.style.configure("BlackText.TButton",foreground="black")
 
         self.recordedEvents = []
 
@@ -25,14 +32,17 @@ class MouseMacro:
 
         #KEYBOARD CONTROLS AND PARAMETERS
         self.keyboardControl = keyboard.Controller()
+        self.keyboardQueue = queue.Queue()
         self.keyboardListener = None
-        self.recordGlobalHotkey = '<ctrl>+a'
-        self.recordGlobalHotkeyListener = keyboard.GlobalHotKeys({
-            self.recordGlobalHotkey: self.recordingStatus
-        })
+        self.recordGlobalHotkey = " "
+        self.globalHotkeyListener = None
         self.isSettingHotkey = False
-        self.keyList = []
-        self.keyHotKeyList = []
+        self.currentSettingHotkey = None
+        self.keyHotkeyList = []
+        self.listString = ""
+        self.recordingControlKey = False
+        self.isCapsLock = False
+        self.isShift = False
 
         #MAIN MACRO CONTROLS
         self.isRecording = False
@@ -49,81 +59,126 @@ class MouseMacro:
         
     
         self.startListener()
+        self.setGlobalHotkeyListener()
         self.processMouseQueue()
+        self.processKeyboardQueue()
         #self.processMouseDisplayQueue()
         self.lastTime = 0
         
 
+        #threading.Thread(target=self.debugKeyEvents, daemon=True).start()
 
         self.root.protocol("WM_DELETE_WINDOW", self.closeApp)
 
 
     def testFunc(self):
+    
         self.keyboardControl.type("this is a test type")
+
+    def debugKeyEvents(self):
+        print("Debug: Listening for raw key events...")
+        with keyboard.Events() as events:
+            for event in events:
+                print(f"Event: {event}")
+
 
     def createFrames(self):
         self.leftFrame = tk.Frame(root)
-        self.leftFrame.columnconfigure(0,weight=1)
-        self.leftFrame.rowconfigure(0,weight=1)
+        self.buttonFrame = tk.Frame(self.leftFrame)
 
-        self.rightFrame = tk.Frame(root,borderwidth=1,relief="solid")
-        self.rightFrame.config(highlightbackground="red",highlightthickness=2)
+        self.rightFrame = tk.Frame(root)
 
     def createWidgets(self):
         #Left Frame
-        self.eventListLabel = tk.Label(self.leftFrame,text="Events")
-        self.eventList = tk.Listbox(self.leftFrame)
-        self.update = tk.Button(self.leftFrame,text="Update")
-        self.clear = tk.Button(self.leftFrame,text="Clear",command=self.clearList)
+        self.eventListLabel = ttk.Label(self.leftFrame,text="Events")
+        self.eventList = tk.Listbox(self.leftFrame,width=50)
+        #self.update = ttk.Button(self.buttonFrame,text="Update")
+        self.clear = ttk.Button(self.buttonFrame,text="Clear",command=self.clearListButton)
         
         #Right Frame
-        self.recordHotkeyLabel = tk.Label(self.rightFrame,text="Set Record Hotkey",bg="white",fg="blue",relief="solid",borderwidth=2)
-        self.recordHotkey = tk.Button(self.rightFrame,text="ctrl+1", command=self.recordHotKeyStatus)
-        self.stopHotkeyLabel = tk.Label(self.rightFrame,text="Stop Hotkey")
-        self.stopHotkey = tk.Button(self.rightFrame,text="ctrl+2")
-        self.record = tk.Button(self.rightFrame,text="Start Recording",command=self.recordingStatus)
-        self.repeatLabel = tk.Label(self.rightFrame,text="Enter number of macro repetitions")
-        self.repeat = tk.Entry(self.rightFrame)
-        self.speedLabel = tk.Label(self.rightFrame,text="Speed")
-        self.speed = tk.OptionMenu(self.rightFrame,"2x","1x")
-        self.start = tk.Button(self.rightFrame,text="Run Macro", command=self.macroController)
-        self.mousePosVar = tk.StringVar()
-        self.mousePosVar.set("Inactive")
-        self.mousePosLabel = tk.Label(self.rightFrame,textvariable=self.mousePosVar)
+        self.recordHotkeyLabel = ttk.Label(self.rightFrame,text="Set Start/Stop Record Hotkey",anchor="center")
+        self.recordHotkey = ttk.Button(self.rightFrame,text="click to set", command= lambda: self.setHotkeyMode("record"))
+        self.record = ttk.Button(self.rightFrame,text="Start Recording",command=self.recordingStatus)
+        self.repeatLabel = ttk.Label(self.rightFrame,text="Enter number of macro repetitions",anchor="center")
+        self.repeat = ttk.Entry(self.rightFrame)
+        self.speedLabel = ttk.Label(self.rightFrame,text="Speed",anchor="center")
+        self.speedVar = tk.StringVar()
+        self.speedVar.set("1x")
+        self.speed = ttk.Combobox(self.rightFrame,textvariable=self.speedVar,values=["1x","2x"],state="readonly",justify="center")
+        self.start = ttk.Button(self.rightFrame,text="Run Macro", command=self.macroController)
+        #self.mousePosVar = tk.StringVar()
+        #self.mousePosVar.set("Inactive")
+        #self.mousePosLabel = tk.Label(self.rightFrame,textvariable=self.mousePosVar)
         self.statusVar = tk.StringVar()
         self.statusVar.set("Status: Idle")
-        self.statusLabel = tk.Label(self.rightFrame,textvariable=self.statusVar)
+        self.statusLabel = ttk.Label(self.rightFrame,textvariable=self.statusVar,anchor="center")
 
     def setGrid(self):
+
+        #root configs
+        root.columnconfigure(0, weight=1)
+        root.columnconfigure(1,weight=0)
+        root.rowconfigure(0,weight=1)
+
         #frames
         self.leftFrame.grid(row=0,column=0, sticky="nsew")
-        self.rightFrame.grid(row=0,column=1, sticky="nsew")
+        self.rightFrame.grid(row=0,column=1, sticky="ns")
+        self.buttonFrame.grid(row=2, column=0, columnspan=2, sticky="ew")
+
+        self.buttonFrame.columnconfigure(0, weight=1)
+        self.buttonFrame.columnconfigure(1, weight=0)
+        self.buttonFrame.columnconfigure(2, weight=1)
+        self.buttonFrame.rowconfigure(0, weight=0)
 
         #Left Frame
-        self.eventListLabel.grid(row=0,column=0)
-        self.eventList.grid(row=1,column=0,columnspan=2)
-        self.update.grid(row=2,column=0)
-        self.clear.grid(row=2,column=1)
-        
+        self.leftFrame.columnconfigure(0, weight=1) 
+        self.leftFrame.columnconfigure(1, weight=1)
+
+        self.leftFrame.rowconfigure(0,weight=0) 
+        self.leftFrame.rowconfigure(1, weight=1)
+        self.leftFrame.rowconfigure(2,weight=0) 
+
+
+        self.eventListLabel.grid(row=0,column=0,columnspan=2,sticky="ew")
+        self.eventList.grid(row=1,column=0,columnspan=2,sticky="nswe")
+        #self.update.grid(row=0, column=1, padx=5, pady=5)
+        self.clear.grid(row=0, column=1)
+
         
         #Right Frame
-        self.recordHotkeyLabel.grid(row=0,column=0)
-        self.recordHotkey.grid(row=1,column=0)
-        self.stopHotkeyLabel.grid(row=0,column=1)
-        self.stopHotkey.grid(row=1,column=1)
-        self.record.grid(row=3,column=0, columnspan=2)
-        self.repeatLabel.grid(row=4, column=0, columnspan=2, pady=(10, 0),sticky="ew")
-        self.repeat.grid(row=5,column=0)
-        self.speedLabel.grid(row=6,column=0)
-        self.speed.grid(row=7,column=0)
-        self.start.grid(row=8,column=0)
-        self.mousePosLabel.grid(row=8,column=1)
-        self.statusLabel.grid(row=9,column=0)
+        self.rightFrame.rowconfigure(8, weight=1)
+        self.rightFrame.rowconfigure(9, weight=0)
+
+        self.recordHotkeyLabel.grid(row=0,column=0,sticky="ew")
+        self.recordHotkey.grid(row=1,column=0,sticky="ew")
+        self.record.grid(row=2,column=0,sticky="ew")
+        self.repeatLabel.grid(row=3, column=0,sticky="ew")
+        self.repeat.grid(row=4,column=0,sticky="ew")
+        self.speedLabel.grid(row=5,column=0,sticky="ew")
+        
+        self.speed.grid(row=6,column=0,sticky="ew")
+        self.start.grid(row=7,column=0,sticky="ew")
+        #self.mousePosLabel.grid(row=8,column=1)
+        self.statusLabel.grid(row=9,column=0,sticky="nsew")
+
+
+    def clearListButton(self):
+        self.clearList()
+        if self.isRecording:
+            self.stopRecording()
 
     def clearList(self):
         self.eventList.delete(0,tk.END)
         self.recordedEvents = []
+       
 
+    def setGlobalHotkeyListener(self):
+        if self.globalHotkeyListener and self.globalHotkeyListener.running:
+            self.globalHotkeyListener.stop()
+
+        self.globalHotkeyListener = keyboard.GlobalHotKeys({
+            self.recordGlobalHotkey:self.recordingStatus})
+        self.globalHotkeyListener.start()
 
     def startListener(self):
         if self.mouseListener is None or not self.mouseListener.is_alive():
@@ -135,61 +190,186 @@ class MouseMacro:
             self.keyboardListener = keyboard.Listener(on_press=self.keyboardPress,
                                                       on_release=self.keyboardRelease)
             self.keyboardListener.start()
-        if not self.recordGlobalHotkeyListener.running:
-            self.recordGlobalHotkeyListener.start()
 
     def keyboardPress(self,key):
+        #print(f"Press: {key} (char: {getattr(key, 'char', 'N/A')})")
         try:
+            print(f"pressed {self.getKeyName(key)}")
+
             if self.isSettingHotkey:
-                self.setRecordHotKey(key);
-            print("alphanumeric key {0} pressed".format(key.char))
-        except AttributeError:
-            print("special key {0} pressed".format(key))
-            #print(f"special key {key} pressed")
-            if key == keyboard.Key.shift:
-                print("shift was pressed")
+                self.collectHotkeyKey(key,"keyPress");
+            elif self.isRecording:
+                self.keyboardQueue.put(("keyPress",key,time.time()))
+                #print(f"testing vk {key.vk}")
+            #print(f"key {key} pressed")
+        except Exception as e:
+            print(f"error when pressing {e}")
     
     def keyboardRelease(self,key):
 
-        if self.isSettingHotkey:
+        try:
+            if self.isSettingHotkey:
+                self.collectHotkeyKey(key,"keyRelease")
+                return;
 
-            print("{0} released".format(key))
-        if key == keyboard.Key.esc:
-            return False
+            if self.isRecording:
+                self.keyboardQueue.put(("keyRelease",key,time.time()))
+
+            print(f"released {self.getKeyName(key)}")
+       
+        except Exception as e:
+            print(f"error when releasing {e}")
         
 
     #sets the hotkey the moment the first key of the list gets released
-    def setRecordHotKey(self,key):
+    """ def setRecordHotkey(self,key):
         self.comboLimit = 3
-        if len(self.keyHotKeyList) < self.comboLimit:
+        if len(self.keyHotkeyList) < self.comboLimit:
             #this is checking if the list is empty
-            keyName = str(key).split(".")[-1]
-            if not self.keyHotKeyList:
+            #keyName = str(key).split(".")[-1]
+            if not self.keyHotkeyList:
                 if isinstance(key,keyboard.Key):
-                    self.keyHotKeyList.append(keyName)
+                    self.keyHotkeyList.append(key)
             else:
-                self.keyHotKeyList.append(keyName)
+                self.keyHotkeyList.append(key)
                 
         else:
             #self.isSettingHotkey = False
             hotkeyName = ""
-            for i in self.keyHotKeyList:
+            for i in self.keyHotkeyList:
                 hotkeyName += i
-            print(hotkeyName)
+            print(hotkeyName) """
 
-
-
-
-    def recordHotKeyStatus(self):
-        if not self.isSettingHotkey:
-            self.recordHotkey.config(text="Set Hotkey",fg="red")
-            self.keyHotKeyList = []
-            self.isSettingHotkey = True
-            #self.recordGlobalHotkey = self.getGlobalHotkey()
-        else:
+    def setHotkeyMode(self, hotkeyType):
+        if self.isSettingHotkey:
             self.isSettingHotkey = False
-            self.recordHotkey.config(text=f"{self.recordGlobalHotkey}",fg="black")
+            self.recordHotkey.config(text=self.recordGlobalHotkey)
+            return
+            #self.recordGlobalHotkey = self.getGlobalHotkey()
 
+        self.isSettingHotkey = True
+        self.currentSettingHotkey = hotkeyType
+        self.keyHotkeyList = []
+
+        if hotkeyType == "record":
+            self.recordHotkey.config(text="Press Keys",style="RedText.TButton")
+            self.statusVar.set("Press keys for record hotkey")
+
+
+    def collectHotkeyKey(self, key, keyType):
+        MAX_KEYS = 3 
+        
+        try:
+            # If we have enough keys or Enter was pressed, finalize the hotkey
+            if len(self.keyHotkeyList) >= MAX_KEYS or keyType == "keyRelease":
+                self.finalizeHotkey()
+            
+            keyName = str(key).split(".")[-1]
+            #print(keyName)
+            if key not in self.keyHotkeyList:
+                self.keyHotkeyList.append((keyType,key))
+                print(f"Added {key} to hotkey")
+                
+            
+                
+        except Exception as e:
+            print(f"Error collecting hotkey: {e}")
+            self.finalizeHotkey()  # Finalize anyway to avoid getting stuck
+
+    def finalizeHotkey(self):
+              
+        # Create hotkey string in pynput format: <ctrl>+a
+        hotkey_str = ""
+        for i, (_, key) in enumerate(self.keyHotkeyList,start=1):
+            
+            #special keys
+            if isinstance(key,keyboard.Key):
+                keyName = str(key).split(".")[-1]
+                if '_' in keyName:
+                    keyName = keyName.split('_')[0]
+                hotkey_str += f"<{keyName}>"
+            else:
+                hotkey_str += key.char
+            
+            if i < len(self.keyHotkeyList):  # Add + between keys
+                hotkey_str += "+"
+        
+        # Apply the hotkey
+        if self.currentSettingHotkey == "record":
+            self.recordGlobalHotkey = hotkey_str
+            self.recordHotkey.config(text=hotkey_str, style="BlackText.TButton")
+
+            
+        # Reset state
+        self.isSettingHotkey = False
+        self.currentSettingHotkey = None
+        self.keyHotkeyList = []
+        
+        #print(self.recordGlobalHotkey)
+        self.statusVar.set(f"Hotkey set to {self.recordGlobalHotkey}")
+        self.setGlobalHotkeyListener()
+
+    def getKeyName(self,key):
+        #CHARS
+        
+        if isinstance(key,keyboard.KeyCode):
+            print(f"vk is {key.vk}")
+            keyName = keyboard.KeyCode.from_vk(int(key.vk))
+            print(f"converted vk {key.vk} is {keyName.char}")
+            return key.char
+        #control keys
+        else:
+            return str(key).split(".")[-1].split('_')[0]
+
+    def getCharUpperLower(self):
+        pass
+
+    def processKeyboardQueue(self):
+        try:
+            while True:
+                eventData = self.keyboardQueue.get_nowait()
+                eventType, key, _ = eventData
+                
+                # Add to recorded events list
+                self.recordedEvents.append(eventData)
+                #print("event data received")
+                
+
+                # Update UI
+                if eventType == "keyPress":
+                    
+                        """ key_name = key.char if hasattr(key, 'char') and key.char else str(key).replace('Key.', '')
+                        self.eventList.insert(tk.END, f"Key press: {key_name}") """
+                        #if is a control key/enter start new string 
+                        
+                        if isinstance(key,keyboard.KeyCode):
+                            self.listString += key.char
+                        else:
+                            self.recordingControlKey = True
+                            keyName = str(key).split(".")[-1]
+                            if self.listString: 
+                                self.eventList.insert(tk.END,f"key press {self.listString}")
+                                self.listString = ""
+                            """ if not self.recordingControlKey:
+                                self.eventList.insert(tk.END,f"key press {keyName}") """
+                            self.listString += keyName
+                #keyRelease
+                else:
+                    if isinstance(key,keyboard.Key):
+                        self.recordingControlKey = False
+                        keyName = str(key).split(".")[-1]
+                        self.eventList.insert(tk.END,f"key press {self.listString}")
+                        self.listString = ""
+              
+
+        except queue.Empty:
+            pass
+        finally:
+            # Schedule the next check
+            self.root.after(100, self.processKeyboardQueue)
+    
+    def setKeyboardString(self,key):
+        pass
 
 
     def mouseClick(self,x,y,button,pressed):
@@ -273,14 +453,14 @@ class MouseMacro:
             print("recording started")
             self.isRecording = True
             self.clearList()
-            self.record.config(text="Stop recording",fg="red")
+            self.record.config(text="Stop recording", style="RedText.TButton")
             
 
     def stopRecording(self):
         if self.isRecording:
             print("recording stopped")
             self.isRecording = False
-            self.record.config(text="Start recording",fg="black")
+            self.record.config(text="Start recording",style="BlackText.TButton")
 
             if self.recordedEvents:
                 lastEvent = self.recordedEvents[-1]
@@ -304,7 +484,7 @@ class MouseMacro:
         
         try:
            self.totalRepetitions = int(self.repeat.get())
-           self.start.config(text="Stop Macro", fg="red")
+           self.start.config(text="Stop Macro", style="RedText.TButton")
         except ValueError:
            print("invalid repetition count")
            self.stopMacro()
@@ -325,7 +505,7 @@ class MouseMacro:
         if self.nextPlayback:
             self.root.after_cancel(self.nextPlayback)
             self.nextPlayback = None
-        self.start.config(text="Run Macro",fg="black")
+        self.start.config(text="Run Macro",style="BlackText.TButton")
         self.statusVar.set("Macro Stopped")
         print("Macro Stopped")
 
@@ -373,6 +553,12 @@ class MouseMacro:
         elif eventType == "move":
             _, x ,y ,_ = currentEvent
             self.mouseControl.position = (x,y)
+        elif eventType == "keyPress":
+            _, key, _ = currentEvent
+            self.keyboardControl.press(key)
+        elif eventType == "keyRelease":
+            _, key, _ = currentEvent
+            self.keyboardControl.release(key)
         
         delay = 10
         nextIndex = self.currentEventIndex + 1
@@ -395,7 +581,7 @@ class MouseMacro:
         self.statusVar.set("macro finished all repetitions")
         self.macroRunning = False
         self.nextPlayback = None
-        self.start.config(text="Run Macro",fg="black")
+        self.start.config(text="Run Macro",style="BlackText.TButton")
 
     def closeApp(self):
         if self.mouseListener and self.mouseListener.is_alive():
--- test.py
+++ test.py
@@ -1,2 +1,32 @@
+from pynput.keyboard import Key, Listener
+import sys
 
+print("\nPress Esc to exit\n")
+print(f'{"form":<10}{"char":<10}{"val":>4}{"code":>8}')
+print(f'{"----":<10}{"----":<10}{"---":>4}{"----":>8}')
 
+lastkey = ""
+def on_press(key):
+    global lastkey
+    if hasattr(key, 'char'):
+        form = "char"
+        char = key.char
+        if char is None:
+            char = val = ""
+        else:
+            val = ord(char)
+        code = key.vk
+        lastkey = ""
+    else:
+        form = "other"
+        char = key.name
+        val = ""
+        code = key.value.vk
+    if lastkey != key:
+        print(f"{form:<10}{char:<10}{val:>4}{code:>8}")
+    lastkey = key
+    if key == Key.esc:
+        sys.exit()
+
+with Listener(on_press=on_press, suppress=True) as listener:
+    listener.join()
